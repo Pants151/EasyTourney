68451a1d1d63712f05129eca6dd47ed346b71983
Jose Antonio V.N <100807059+Pants151@users.noreply.github.com>
2026-02-27 23:39:26 +0100

    Actualiza el nombre de la aplicación en el manifiesto, agrega soporte para Service Worker y mejora la gestión de caché

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'easytourney-cache-v2'; // <- Hemos subido a v2
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico'
+];
+
+// Evento Install
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Archivos cacheados con éxito');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Evento Fetch: Intercepta las peticiones de la app
+self.addEventListener('fetch', (event) => {
+  // Si la petición es para Socket.io o para nuestra API del Backend,
+  // no hacemos nada y dejamos que el navegador la maneje normalmente.
+  if (event.request.url.includes('/socket.io/') || event.request.url.includes('/api/')) {
+    return; // Salimos de la función
+  }
+
+  // Para el resto (imágenes, CSS, HTML), buscamos en la caché
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      }).catch(() => {
+        // Si falla todo (no hay internet y no está en caché)
+        console.log('Fallo en la red y no hay caché para:', event.request.url);
+      })
+  );
+});
+
+// Evento Activate
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName); // Borra la v1 antigua
+          }
+        })
+      );
+    })
+  );
+});
