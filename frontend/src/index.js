edeba37bb192d1ac320ace7da7a9b7cb71a4edc2
Jose Antonio V.N <100807059+Pants151@users.noreply.github.com>
2026-03-15 21:20:43 +0100

    Initialize React application with React DOM and register service worker.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -24,5 +23,3 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
-
-reportWebVitals();
